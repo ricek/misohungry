1969467d1dfdcc8782f37b8a542f354567c25c85
GanixiL <xiangli1997@gmail.com>
2014-11-11 15:58:42 -0500

    update database 2
    update the number of desserts to 10

--- data/FDD.js
+++ data/FDD.js
@@ -129,5 +129,75 @@ var FDD = {
         "Pour custard into caramel lined ramekins. Place ramekins in a large glass or ceramic baking dish and fill with about 1-2 inches of hot water. Bake for 45 minutes in the water bath and check with a knife just to the side of the center. If knife comes out clean, it's ready.",
         "Remove and let cool. Let each ramekin cool in refrigerator for 1 hour. Invert each ramekin onto a small plate, the caramel sauce will flow over the custard."]
     },
+    {
+    	"name":"Chunky Cheesecake Brownies",
+    	"overview":"1 (8 ounce) package cream cheese, softened<br>1/4 cup white sugar<br>1 egg<br>1 cup semisweet chocolate chips<br>1/4 cup butter<br>1 cup semisweet chocolate chips<br>1/2 cup white sugar<br>2 eggs<br>2/3 cup all-purpose flour<br>1/2 teaspoon baking powder<br>1/4 teaspoon salt",
+    	"steps":[
+    		"Preheat oven to 350 degrees F (175 degrees C). Grease a 9 inch square baking pan.",
+    		"Combine cream cheese with 1/4 cup sugar and 1 egg in a mixing bowl; beat until smooth. Stir 1 cup chocolate chips into the cream cheese mixture. Set aside.",
+    		"Fill a saucepan with water and bring to a boil. Turn the heat off, and set a heatproof mixing bowl over the water. In the mixing bowl, combine butter with the remaining cup of chocolate chips; stir until just melted and blended together. Stir in the remaining 1/2 cup sugar and 2 eggs, then sift together flour, baking powder, and salt; stir into chocolate until evenly blended.",
+    		"Pour half of the batter into the prepared baking pan. Spread the cream cheese mixture over the chocolate layer. Top with remaining chocolate mixture (this doesn't need to completely cover the cream cheese layer). Using a knife, swirl the top chocolate layer into the cream cheese to make a marble pattern.",
+    		"Bake in preheated oven at 350 degrees F (175 degrees C) for 25 to 30 minutes, or until top is crinkled and edges pull away from sides of the pan. Cool thoroughly. Cut into 12 to 16 squares. Store in the refrigerator or freeze."
+    	]
+    },
+     {
+    	"name":"Cottage Cheese Fluff",
+    	"overview":"3 cups low-fat cottage cheese<br>2 (0.3 ounce) packages sugar-free lemon flavored Jell-O® mix<br>1 (8 ounce) container lite frozen whipped topping, thawed",
+    	"steps":["Place the cottage cheese in a food processor and blend until creamy. Whisk in the flavored gelatin powder, then fold in the thawed whipped topping. Refrigerate until serving."]
+    },
+     {
+    	"name":"Chocolate-Hazelnut Spread Cookies",
+    	"overview":"1 cup graham cracker crumbs<br>1/2 teaspoon baking soda<br>1 pinch salt<br>1 egg<br>1/2 teaspoon vanilla extract<br>1 cup chocolate-hazelnut spread (such as Nutella®)",
+    	"steps":[
+    		"Preheat an oven to 350 degrees F (175 degrees C).",
+    		"Combine the graham cracker crumbs, baking soda, and salt in a large bowl; add the egg, vanilla, and chocolate-hazelnut spread and mix by hand until well blended. Form the mixture into 1 1/2-inch balls and place on a baking sheet 2 inches apart.",
+    		"Bake in the preheated oven until crispy on the outside, 8 to 10 minutes. Allow to rest on cookie sheet a few minutes before moving to a wire rack to cool."
+    	]
+    },
+     {
+    	"name":"Virginia Apple Pudding",
+    	"overview":"1/2 cup butter, melted<br>1 cup white sugar<br>1 cup all-purpose flour<br>2 teaspoons baking powder<br>1/4 teaspoon salt<br>1 cup milk<br>2 cups chopped, peeled apple<br>1 teaspoon ground cinnamon",
+    	"steps":[
+    		"Preheat oven to 375 degrees F (190 degrees C).",
+    		"In a small baking dish, combine butter, sugar, flour, baking powder, salt, and milk until smooth.",
+    		"In a microwave-safe bowl, combine apples and cinnamon. Microwave until apples are soft, 2 to 5 minutes. Pour apples into the center of the batter.",
+    		"Bake in the preheated oven 30 minutes, or until golden."
+    	]
+    },
+     {
+    	"name":"Best Peanut Butter Cookies Ever",
+    	"overview":"2 cups peanut butter<br>2 cups white sugar<br>2 eggs<br>2 teaspoons baking soda<br>1 pinch salt<br>1 teaspoon vanilla extract",
+    	"steps":[
+    		"Preheat oven to 350 degrees F (175 degrees C). Grease cookie sheets.",
+    		"In a medium bowl, stir peanut butter and sugar together until smooth. Beat in the eggs, one at a time, then stir in the baking soda, salt, and vanilla. Roll dough into 1 inch balls and place them 2 inches apart onto the prepared cookie sheets. Press a criss-cross into the top using the back of a fork.",
+    		"Bake for 8 to 10 minutes in the preheated oven. Allow cookies to cool on baking sheet for 5 minutes before removing to a wire rack to cool completely."
+    	]
+    },
+     {
+    	"name":"Blackberry Cobbler",
+    	"overview":"1 cup all-purpose flour<br>1 1/2 cups white sugar, divided<br>1 teaspoon baking powder<br>1/2 teaspoon salt<br>6 tablespoons cold butter<br>1/4 cup boiling water<br>2 tablespoons cornstarch<br>1/4 cup cold water<br>1 tablespoon lemon juice<br>4 cups fresh blackberries, rinsed and drained",
+    	"steps":[
+    		"Preheat oven to 400 degrees F (200 degrees C). Line a baking sheet with aluminum foil.",
+    		"In a large bowl, mix the flour, 1/2 cup sugar, baking powder, and salt. Cut in butter until the mixture resembles coarse crumbs. Stir in 1/4 cup boiling water just until mixture is evenly moist.",
+    		"In a separate bowl, dissolve the cornstarch in cold water. Mix in remaining 1 cup sugar, lemon juice, and blackberries. Transfer to a cast iron skillet, and bring to a boil, stirring frequently. Drop dough into the skillet by spoonfuls. Place skillet on the foil lined baking sheet.",
+    		"Bake 25 minutes in the preheated oven, until dough is golden brown."
+    	]
+    },
+     {
+    	"name":"Chocolate Delight",
+    	"overview":"2 (8 or 9 inch) baked round chocolate cake layers, cooled<br>1 cup coffee-flavored liqueur<br>2 (5.9 ounce) packages instant chocolate pudding mix<br>4 cups milk, or as needed<br>6 (1.4 ounce) bars chocolate covered toffee bars, chopped<br>2 (12 ounce) containers frozen whipped topping, thawed<br>1 (1 ounce) square semisweet chocolate",
+    	"steps":[
+    		"Cut chocolate cake into large squares and place on a baking sheet or large baking dish (see Cook's Note). Pour coffee-flavored liqueur over cake and set aside. Prepare chocolate pudding with milk according to package directions.",
+    		"In large trifle bowl or other glass bowl, crumble half of liqueur-moistened cake. Top cake with half of chocolate pudding, half of crumbled candy bars, and one container of whipped topping. Repeat with remaining cake, pudding, candy bars, and Cool Whip®. Shave chocolate with a vegetable peeler for garnish; sprinkle over top of dessert. Keep refrigerated until serving."
+    	]
+    },
+     {
+    	"name":"Banana Pudding",
+    	"overview":"1 (8 ounce) package cream cheese<br>1 (14 ounce) can sweetened condensed milk<br>1 (5 ounce) package instant vanilla pudding mix<br>3 cups cold milk<br>1 teaspoon vanilla extract<br>1 (8 ounce) container frozen whipped topping, thawed<br>4 bananas, sliced<br>1/2 (12 ounce) package vanilla wafers",
+    	"steps":[
+    		"In a large bowl, beat cream cheese until fluffy. Beat in condensed milk, pudding mix, cold milk and vanilla until smooth. Fold in 1/2 of the whipped topping.",
+    		"Line the bottom of a 9x13 inch dish with vanilla wafers. Arrange sliced bananas evenly over wafers. Spread with pudding mixture. Top with remaining whipped topping. Chill."
+    	]
+    },
     ]
 }
